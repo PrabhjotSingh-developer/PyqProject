b80ddc5f16671fc3b16aa80c5727e360c4365d4c
aman <webwithaman06@gmail.com>
2024-01-28 16:09:03 +0530

    add : navbar complete -aman

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,42 @@
-import React from 'react'
-import './Navbar.css' //css added 
+import React from "react";
+import "./Navbar.css"; // css added
 // import  {Link}  from 'react-router-dom' //anchor gi jagah
-import { useContext } from 'react'
-import MyContext from '../../context/data/MyContext.jsx'
+import { useContext } from "react";
+import MyContext from "../../context/data/MyContext.jsx";
 const Navbar = () => {
   const context = useContext(MyContext);
-  const {mode,toggleMode} = context;
-  console.log(context)
+  const { mode, toggleMode } = context;
+  console.log(context);
   return (
-    
-
+    // navbbar
     <div className={`navbar`}>
-        
-        <nav className='navbar-container d-flex align-center justify-between'>
+      {/* navbar-container */}
+      <nav className="navbar-container d-flex align-center justify-between">
+        {/* logo */}
+        <div className="logo-box">
+          <h1 className="logo">Paper Dekho</h1>
+        </div>
 
-            <div className='logo-box'>
-              <h1 className='logo'>Paper Dekho</h1>
-            </div>
-
-            <ul className='nav-links'>
-              <li><a href="">Home</a></li>
-              <li><a href="">About</a></li>
-              <li><a href="">Contact</a></li>
-              <li><a href="">Admin</a></li>
-            </ul>
-
-        </nav>
-       
+        {/* nav-links */}
+        <ul className="nav-links d-flex align-center">
+          <li>
+            <a className="active-nav-link" href="">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+          <li>
+            <a href="">Admin</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-} 
+  );
+};
 
-export default Navbar
+export default Navbar;
