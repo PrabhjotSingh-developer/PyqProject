ae6ed85c6adedbd6dd35b744273b1abecb2355fd
aman <webwithaman06@gmail.com>
2024-01-28 14:54:12 +0530

    add : navbar Navbar.jsx -aman

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,12 +3,22 @@ import './Navbar.css' //css added
 import { Link } from 'react-router-dom' //anchor gi jagah
 const Navbar = () => {
   return (
-    <div className="navbar">
+
+    <div className={`navbar`}>
         
-        <nav className="">
+        <nav className="navbar-container">
+
+            <div className="logo-box"></div>
+
+            <ul className={`nav-links`}>
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Admin</a></li>
+            </ul>
+
 
         </nav>
-
        
     </div>
   )
