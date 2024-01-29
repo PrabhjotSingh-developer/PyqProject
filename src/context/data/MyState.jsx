3666e56b441bf35d7d6faf318ccd54cafbfc2629
PrabhjotSingh-developer <prabhjotsingh1032003@gmail.com>
2024-01-29 11:18:32 +0530

    adding light mode classes

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -3,19 +3,21 @@ import { useState } from 'react'
 import MyContext from './MyContext.jsx'
 const MyState = (props) => {
     const [mode,setMode] = useState('light');
+    const html = document.getElementsByTagName("html") 
+
     const toggleMode = () =>{
         if(mode === 'light')
         {
-        setMode('dark')
-        document.getElementsByTagName("html")
-        
-
+         setMode('dark')
+          html[0].classList.remove("light-mode")
+          html[0].classList.add("dark-mode")
+          console.log("yes work")
         }
       else
         {
-                 document.getElementsByTagName("html")
-
-        setMode('light')
+          html[0].classList.add("light-mode")
+          html[0].classList.remove("dark-mode")
+          setMode('light')
        }
     }
   return (
