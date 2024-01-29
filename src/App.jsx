8efc7d4e9fca2334e0fe1961c8385c3a1d5c405f
aman <webwithaman06@gmail.com>
2024-01-29 12:26:47 +0530

    add : refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/navbar/Navbar.jsx'
-import Footer from './components/footer/Footer.jsx'
-import MyState from './context/data/myState.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import MyState from "./context/data/myState.jsx";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     // <MyState>
@@ -15,7 +15,7 @@ function App() {
     //      <Footer/>
     // </MyState>
     <></>
-  )
+  );
 }
 
-export default App
+export default App;
