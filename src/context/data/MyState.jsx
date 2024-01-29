592317e2edb283d463913a1446e3768668885ade
aman <webwithaman06@gmail.com>
2024-01-29 11:20:28 +0530

    add : refactor

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -1,31 +1,27 @@
-import React from 'react'
-import { useState } from 'react'
-import MyContext from './MyContext.jsx'
+import React from "react";
+import { useState } from "react";
+import MyContext from "./MyContext.jsx";
 const MyState = (props) => {
-    const [mode,setMode] = useState('light');
-    const html = document.getElementsByTagName("html") 
+  const [mode, setMode] = useState("light");
+  const html = document.getElementsByTagName("html")[0];
 
-    const toggleMode = () =>{
-        if(mode === 'light')
-        {
-         setMode('dark')
-          html[0].classList.remove("light-mode")
-          html[0].classList.add("dark-mode")
-          console.log("yes work")
-        }
-      else
-        {
-          html[0].classList.add("light-mode")
-          html[0].classList.remove("dark-mode")
-          setMode('light')
-       }
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      html.classList.remove("light-mode");
+      html.classList.add("dark-mode");
+      console.log("yes work");
+    } else {
+      html.classList.add("light-mode");
+      html.classList.remove("dark-mode");
+      setMode("light");
     }
+  };
   return (
-    <MyContext.Provider value = {{mode,toggleMode}}>
-        {props.children}
+    <MyContext.Provider value={{ mode, toggleMode }}>
+      {props.children}
     </MyContext.Provider>
-  )
-}
+  );
+};
 
-export default MyState
- 
+export default MyState;
